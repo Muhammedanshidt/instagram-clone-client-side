99aaafe1d641655fa9f6e63c4ebda773c14b5aae
Anshid <muhammedanshidh.t@gmail.com>
2024-03-20 10:20:40 +0530

    deploy 9th

--- src/pages/user/CreatPost.jsx
+++ src/pages/user/CreatPost.jsx
@@ -4,13 +4,7 @@ export default function Modal() {
   const [showModal, setShowModal] = React.useState(false);
   return (
     <>
-      {/* <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
-        Open small modal
-      </button> */}
+   
       {showModal ? (
         <>
           <div
