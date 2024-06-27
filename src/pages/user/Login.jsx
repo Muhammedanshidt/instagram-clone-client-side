1f455d72aef92030c8ee6131a13c00aa1092cea0
Anshid <muhammedanshidh.t@gmail.com>
2024-06-27 16:46:22 +0530

    125

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -75,41 +75,40 @@ function Login() {
     }
   };
   return (
-    <>
-      <div className="container">
-        <div className="loginImage">
+    <div className=" h-[100vh] flex flex-col justify-center items-center  ">
+        {/* <div className="loginImage p-5 ">
           <img
             className=" mt-12"
             src={LoginImage}
-            style={{ width: "450px", height: "581px" }}
+            style={{ width: "450px", height: "581px" ,display:"none"}}
             alt=""
           />
-        </div>
-        <div className="loginInput ">
-          <div className="mainInputBox sm:flex items-center">
-            <div className="titleImage">
+        </div> */}
+        
+        {/* <div className=" h-fit felx flex-col justify-center items-center p-2 border border-1 border-gray-300"> */}
+  
               <img
+               className="w-40"
                 src={TitleImage}
                 alt="Logo"
-                style={{ width: "175px", height: "50px" }}
               />
-            </div>
+            <div className="h-fit flex flex-col justify-center items-center  ">
             <input
               placeholder=" email address"
-              className="inputBox mt-16"
+              className="inputBox mt-16 font-custom_input"
               type="text"
               onChange={(e) => setEmail(e.target.value)}
             />
             <br />
             <input
               placeholder="password"
-              className="inputBox "
+              className="inputBox font-custom_input "
               type="password"
               onChange={(e) => setPassword(e.target.value)}
             />
             <br />
             <button
-              className="bg-blue-400 hover:bg-blue-600 text-white font-bold rounded lg:px-24 lg:py-1 md:px-6 md:py-3"
+              className="bg-blue-400 hover:bg-blue-600 text-white font-bold rounded px-24"
               onClick={handleSubmit}
             >
               Log in
@@ -119,17 +118,17 @@ function Login() {
               OR
             </p>
             <a
-              href="https://www.facebook.com/login.php?skip_api_login=1&api_key=124024574287414&kid_directed_site=0&app_id=124024574287414&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Foauth%3Fclient_id%3D124024574287414%26locale%3Den_GB%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Faccounts%252Fsignup%252F%26response_type%3Dcode%252Cgranted_scopes%26scope%3Demail%26state%3D%257B%2522fbLoginKey%2522%253A%2522m5llen77dfex1gzxfoc1spf56h1hok6jv1kt9qfz17alw5z16ssd75%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252F%2522%257D%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3D7ce240d0-8907-4923-9979-d02626257204%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fwww.instagram.com%2Faccounts%2Fsignup%2F%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%2522fbLoginKey%2522%253A%2522m5llen77dfex1gzxfoc1spf56h1hok6jv1kt9qfz17alw5z16ssd75%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252F%2522%257D%23_%3D_&display=page&locale=en_GB&pl_dbl=0"
+              href="https://www.facebook.com/.php?skip_api_login=1&api_key=124024574287414&kid_directed_site=0&app_id=124024574287414&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Foauth%3Fclient_id%3D124024574287414%26locale%3Den_GB%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Faccounts%252Fsignup%252F%26response_type%3Dcode%252Cgranted_scopes%26scope%3Demail%26state%3D%257B%2522fbLoginKey%2522%253A%2522m5llen77dfex1gzxfoc1spf56h1hok6jv1kt9qfz17alw5z16ssd75%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252F%2522%257D%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3D7ce240d0-8907-4923-9979-d02626257204%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fwww.instagram.com%2Faccounts%2Fsignup%2F%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%2522fbLoginKey%2522%253A%2522m5llen77dfex1gzxfoc1spf56h1hok6jv1kt9qfz17alw5z16ssd75%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252F%2522%257D%23_%3D_&display=page&locale=en_GB&pl_dbl=0"
               // target="_blank"
               // rel="noopener noreferrer"
             >
-              <p className="mt-6 text-sm">Log in with Facebook</p>
+              {/* <p className="mt-6 text-sm">Log in with Facebook</p> */}
             </a>
-            <p className="text-green-900 text-xs mt-6">
+            {/* <p className="text-green-900 text-xs mt-6">
               Forgotten your password?
-            </p>
+            </p> */}
           </div>
-          <div className="w-[350px] h-16 border border-1border-gray-300 flex justify-center items-center">
+          <div className="w-fit px-8 h-16 border border-1 border-gray-300 flex justify-center items-center">
             <p className="text-sm ">
               {" "}
               Don't have an account?{" "}
@@ -141,9 +140,9 @@ function Login() {
               </span>{" "}
             </p>
           </div>
-          <div className="w-[350px] h-16">
-            <p className="text-center text-sm">Get the app.</p>
-            <div className=" flex gap-3 flex justify-center mt-6 ">
+          <div className="w-fit h-16 ">
+            <p className="text-sm text-center">Get the app.</p>
+            <div className=" flex gap-3 justify-center mt-2 ">
               <a
                 href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=ig_mid%3DBB6917D7-37B1-435C-92DA-4A0D28DBC10A%26utm_campaign%3DloginPage%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge&pli=1"
                 target="_blank"
@@ -168,9 +167,7 @@ function Login() {
               </a>
             </div>
           </div>
-        </div>
-      </div>
-    </>
+          </div>
   );
 }
 export default Login;
--- src/pages/user/SignUp.jsx
+++ src/pages/user/SignUp.jsx
@@ -80,154 +80,154 @@ function SignUp() {
     }
   };
   return (
-    <div className="flex justify-center items-center flex-col">
-      <div
-        className="w-[350px] h-[600px] mt-3 flex items-center 
-    border border-1 border-gray-300 flex-col "
+    <div className="h-screen flex justify-center items-center flex-col md:mt-16">
+    <div
+      className="w-[350px] h-[600px] mt-3 
+      border border-1 border-gray-300 flex-col sm flex items-center justify-center "
+    >
+      <div className="">
+        <img
+          className="mt-12"
+          src={TitleImage}
+          alt="Logo"
+          style={{ width: "175px", height: "50px" }}
+        />
+      </div>
+      <div>
+        <h3 className="text-center text-base mt-4 font-semibold text-gray-500 leading-4">
+          Sign up to see photos and videos
+          <br />
+          from your friends.
+        </h3>
+      </div>
+      <button className="bg-blue-500 hover:bg-blue-600 text-white text-sm font-semibold rounded-md w-fit px-10 py-1 mt-3">
+        Log in with Facebook
+      </button>
+      <p className=" mt-4 text-gray-500 lg:px-24 lg:py-1 md:px-6 md:py-3 text-sm font-semibold">
+        OR
+      </p>
+      <div className=" flex-col ">
+        <input
+          onChange={handleChange}
+          className="signInput mt-2 "
+          type="email"
+          placeholder="Email address"
+          id="email"
+          name="email"
+          required
+          autoFocus
+        />
+        <br />
+        <input
+          onChange={handleChange}
+          className="signInput mt-2"
+          type="text"
+          placeholder="Full Name"
+          id="name"
+          name="fullname"
+          required
+        />
+        <br />
+        <input
+          onChange={handleChange}
+          className="signInput mt-2 "
+          type="text"
+          placeholder="username"
+          id="user"
+          name="username"
+          required
+        />
+        <br />
+        <input
+          onChange={handleChange}
+          className="signInput mt-2"
+          type="password"
+          placeholder="Password"
+          id="password"
+          name="password"
+          required
+        />
+      </div>
+      <div className="w-[268px] h-[100px] ">
+        <div className="text-xs text-center mt-2">
+          People who use our service may have uploaded your contact
+          information to Instagram.
+          <a
+            href="https://www.facebook.com/help/instagram/261704639352628"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="linkSpan">Learn more</span>
+          </a>
+        </div>
+        <div className="text-xs text-center mt-4">
+          By signing up, you agree to our{" "}
+          <a
+            href="https://help.instagram.com/581066165581870/?locale=en_GB"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <span className="linkSpan">Terms</span>
+          </a>
+          ,
+          <a
+            href="https://www.facebook.com/privacy/policy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="linkSpan">Privacy Policy</span>
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://privacycenter.instagram.com/policies/cookies/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="linkSpan">Cookies Policy</span>.
+          </a>
+        </div>
+      </div>
+      <button
+        onClick={sumbitForm}
+        className="bg-blue-400 text-white text-sm font-semibold rounded-md w-fit px-10 py-2 mb-2 cursor-default"
       >
-        <div className="">
-          <img
-            className="mt-12"
-            src={TitleImage}
-            alt="Logo"
-            style={{ width: "175px", height: "50px" }}
-          />
-        </div>
-        <div>
-          <h3 className="text-center text-base mt-4 font-semibold text-gray-500 leading-4">
-            Sign up to see photos and videos
-            <br />
-            from your friends.
-          </h3>
-        </div>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white text-sm font-semibold rounded-md lg:px-[80px] lg:py-2 md:px-6 md:py-3 mt-6">
-          Log in with Facebook
-        </button>
-        <p className=" mt-4 text-gray-500 lg:px-24 lg:py-1 md:px-6 md:py-3 text-sm font-semibold">
-          OR
-        </p>
-        <div className=" flex-col ">
-          <input
-            onChange={handleChange}
-            className="signInput mt-2 "
-            type="email"
-            placeholder="Email address"
-            id="email"
-            name="email"
-            required
-            autoFocus
-          />
-          <br />
-          <input
-            onChange={handleChange}
-            className="signInput mt-2"
-            type="text"
-            placeholder="Full Name"
-            id="name"
-            name="fullname"
-            required
-          />
-          <br />
-          <input
-            onChange={handleChange}
-            className="signInput mt-2 "
-            type="text"
-            placeholder="username"
-            id="user"
-            name="username"
-            required
-          />
-          <br />
-          <input
-            onChange={handleChange}
-            className="signInput mt-2"
-            type="password"
-            placeholder="Password"
-            id="password"
-            name="password"
-            required
-          />
-        </div>
-        <div className="w-[268px] h-[100px] ">
-          <div className="text-xs text-center mt-2">
-            People who use our service may have uploaded your contact
-            information to Instagram.
-            <a
-              href="https://www.facebook.com/help/instagram/261704639352628"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="linkSpan">Learn more</span>
-            </a>
-          </div>
-          <div className="text-xs text-center mt-4">
-            By signing up, you agree to our{" "}
-            <a
-              href="https://help.instagram.com/581066165581870/?locale=en_GB"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {" "}
-              <span className="linkSpan">Terms</span>
-            </a>
-            ,
-            <a
-              href="https://www.facebook.com/privacy/policy"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="linkSpan">Privacy Policy</span>
-            </a>{" "}
-            and{" "}
-            <a
-              href="https://privacycenter.instagram.com/policies/cookies/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="linkSpan">Cookies Policy</span>.
-            </a>
-          </div>
-        </div>
-        <button
-          onClick={sumbitForm}
-          className="bg-blue-400 text-white text-sm font-semibold rounded-md lg:w-[200px] lg:py-2 md:py-2 sm:py-2 sm:w-20 cursor-default"
+        Sign Up
+      </button>
+    </div>
+    <div className="w-[350px] py-3 h-16 border border-1border-gray-300 flex justify-center items-center mt-2">
+      <p className="text-sm ">
+        {" "}
+        have an account?{" "}
+        <span
+          className="text-sm font-medium text-blue-500 cursor-pointer"
+          onClick={() => routeNavigate("/")}
         >
-          Sign Up
-        </button>
-      </div>
-      <div className="w-[350px] h-16 border border-1border-gray-300 flex justify-center items-center mt-2">
-        <p className="text-sm ">
-          {" "}
-          have an account?{" "}
-          <span
-            className="text-sm font-medium text-blue-500 cursor-pointer"
-            onClick={() => routeNavigate("/login")}
-          >
-            Log in
-          </span>{" "}
-        </p>
-      </div>
+          Log in
+        </span>{" "}
+      </p>
+    </div>
 
-      <div className="w-[350px] h-18 mt-2">
-        <p className="text-center text-sm">Get the app.</p>
-        <div className="flex gap-3 flex justify-center my-6">
-          <a
-            href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=ig_mid%3DBB6917D7-37B1-435C-92DA-4A0D28DBC10A%26utm_campaign%3DloginPage%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge&pli=1"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img className="h-[40px]" src={PlayStore} alt="playstore logo" />
-          </a>
-          <a
-            href="https://apps.microsoft.com/detail/9NBLGGH5L9XT?hl=en-US&gl=US"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img className="h-[40px]" src={Microsoft} alt="playstore logo" />
-          </a>
-        </div>
+    <div className="w-[350px] h-18 mt-2">
+      <p className="text-center text-sm">Get the app.</p>
+      <div className="flex gap-3  justify-center my-6">
+        <a
+          href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=ig_mid%3DBB6917D7-37B1-435C-92DA-4A0D28DBC10A%26utm_campaign%3DloginPage%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge&pli=1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="h-[40px]" src={PlayStore} alt="playstore logo" />
+        </a>
+        <a
+          href="https://apps.microsoft.com/detail/9NBLGGH5L9XT?hl=en-US&gl=US"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="h-[40px]" src={Microsoft} alt="playstore logo" />
+        </a>
       </div>
     </div>
+  </div>
   );
 }
 
